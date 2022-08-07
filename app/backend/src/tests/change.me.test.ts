386831d4d44d8e5ede74a2cb69805980e7988b38
Kelly Cassiano <keukssie@hotmail.com>
2022-08-07 01:02:57 -0300

    feat: testes

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -4,42 +4,133 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
-
-import { Response } from 'superagent';
+import User from '../database/models/User';
+import * as jwt from 'jsonwebtoken';
 
 chai.use(chaiHttp);
-
 const { expect } = chai;
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+describe('Login realizado com sucesso', () => {
 
-  // let chaiHttpResponse: Response;
+  before(async () => {
+    
+    const mock = {
+      username: 'Admin',
+      role: 'admin',
+      email: 'admin@admin.com',
+      password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+    }  as User
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
+    
+    sinon.stub(User, "findOne").resolves(mock);
+  });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+  after(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
+  it('Rota post "/login" realizado com sucesso', async () => {
+  const { status, body } =  await chai.request(app).post('/login').send({
+    email: 'admin@admin.com',
+    password: 'secret_admin'
+  });
+  expect(status).to.be.eq(200);
+  expect(Object.keys(body)).to.deep.eq(['token']);
+  });
 
-  //   expect(...)
-  // });
+  it('verificando token da rota POST login', async () => {
+    const { body: { token } } =  await chai.request(app).post('/login').send({
+      email: 'admin@admin.com',
+      password: 'secret_admin'
+    });
+    expect(token).to.be.a('string');
+    const { username, role, email, password } = jwt
+      .decode(token) as { username: string, role: string, email: string, password: string };
+    expect(username).to.be.eq('Admin');
+    expect(role).to.be.eq('admin');
+    expect(email).to.be.eq('admin@admin.com');
+    expect(password).to.be.eq(undefined);
+  });
+});
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+
+describe('Login com falha', () => {
+
+  it('Rota POST "/login" com email inexistente', async () => {
+    const mock = null
+    sinon.stub(User, "findOne").resolves(mock);
+      const { status, body: { message } } =  await chai.request(app).post('/login').send({
+        email: 'admin@admin.com',
+        password: 'secret_admin'
+      });
+    expect(status).to.be.eq(401);
+    expect(message).to.be.eq('Incorrect email or password');
+    (User.findOne as sinon.SinonStub).restore();
+  });
+
+  it('Rota POST "/login" com senha incorreta', async () => {
+    const mock = {
+      username: 'Admin',
+      role: 'admin',
+      email: 'admin@admin.com',
+      password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+    }  as User
+    sinon.stub(User, "findOne").resolves(mock);
+      const { status, body: { message } } =  await chai.request(app).post('/login').send({
+        email: 'admin@admin.com',
+        password: 'secret_user'
+      });
+    expect(status).to.be.eq(401);
+    expect(message).to.be.eq('Incorrect email or password');
+    (User.findOne as sinon.SinonStub).restore();
+  });
+
+  it('Rota POST "/login" sem o campo password', async () => {
+    const { status, body: { message } } =  await chai.request(app).post('/login').send({
+        email: 'admin@admin.com'
+      });
+    expect(status).to.be.eq(400);
+    expect(message).to.be.eq('All fields must be filled');
+  });
+
+  it('Rota POST "/login" sem o campo email', async () => {
+      const { status, body: { message } } =  await chai.request(app).post('/login').send({
+        password: 'secret_admin'
+      });
+    expect(status).to.be.eq(400);
+    expect(message).to.be.eq('All fields must be filled');
+  });
+});
+
+describe('testando login validate', () => {
+  before(async () => {
+    const mock = {
+      username: 'Admin',
+      role: 'admin',
+      email: 'admin@admin.com',
+      password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+    }  as User
+    sinon.stub(User, "findOne").resolves(mock);
+  });
+
+  after(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  it('login validate com sucesso', async () => {
+    const { body: { token } } = await chai.request(app).post('/login').send({
+      email: 'admin@admin.com',
+      password: 'secret_admin'
+    });
+    const { status, body } = await chai.request(app).get('/login/validate').set({ Authorization: token });
+    expect(status).to.be.eq(200);
+    expect(body).to.deep.eq({ role: 'admin' })
+  })
+
+  it('login validate com falha', async () => {
+    const token = '123456789';
+    const { status, body } = await chai.request(app).get('/login/validate').set({ Authorization: token });
+    expect(status).to.be.eq(401);
+    expect(body).to.deep.eq({ message: 'Token must be a valid token' });
   });
 });
